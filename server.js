84d7e6fed1e6eaf04ee0a9e6c3be91a56eddf47e
Mohammad Raza Rupani <mohammadrazarupani@gmail.com>
2026-07-09 23:09:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post("/send-dm", async (req, res) => {
             timeout: 40000,
           });
 
-          await page.waitForTimeout(720000);
+          await page.waitForTimeout(10000);
 
           const messageButton = page.getByRole("button", {
             name: "Message",
